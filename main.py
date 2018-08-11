9a999ca4c20cb956ddc83d9b5806239f8e61e569
Or Bin <orbin50@gmail.com>
2018-08-11 20:08:19 +0300

    Added card creation in Trello. The script is practically ready now.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import requests
 from pocket import Pocket
 from datetime import datetime
 from bs4 import BeautifulSoup
+from trello import TrelloClient
 
 
 CONFIG_FILE_NAME = 'config.json'
@@ -16,31 +17,43 @@ auth_data = conf_data[AUTH_DATA_KEY]
 
 # Logging in to Pocket
 pocket_consumer_key = conf_data[AUTH_DATA_KEY]['pocket_consumer_key']
-pocket_instance = Pocket(conf_data[AUTH_DATA_KEY]['pocket_consumer_key'],
-                         conf_data[AUTH_DATA_KEY]['pocket_user_credentials']['access_token'])
+pocket_client = Pocket(conf_data[AUTH_DATA_KEY]['pocket_consumer_key'],
+                       conf_data[AUTH_DATA_KEY]['pocket_user_credentials']['access_token'])
 
 # Logging in to Trello
-# TODO
+trello_client = TrelloClient(
+    api_key='7413612d698d185f50b2e775be180abe',
+    token='8a3e786371ceeba2d9f650a9a587772fce41a6fc7dccc7ad1747b6c402982494'
+)
+trello_list = trello_client.get_list(conf_data['trello_list_id'])
 
 now_timestamp = int(datetime.now().timestamp())
 since_timestamp = conf_data['pocket_last_checked'] if 'pocket_last_checked' in conf_data else now_timestamp
 
-response_data, _ = pocket_instance.get(since=since_timestamp)
+new_pocket_items, _ = pocket_client.get(since=since_timestamp)
 
-if len(response_data['list']) == 0:
+if len(new_pocket_items['list']) == 0:
     print('No new items.')
     sys.exit(0)
 
-for item_id, item_data in response_data['list'].items():
-    with requests.get(item_data['given_url']) as page_response:
-        parsed_page = BeautifulSoup(page_response.text, 'html.parser')
-    page_title = parsed_page.title.text
+for pocket_item_id, pocket_item_data in new_pocket_items['list'].items():
 
-    print(page_title)
-    print(item_data['given_url'])
-    print(item_data['excerpt'])
+    # Getting page title
+    page_url = pocket_item_data['given_url']
+    if page_url.endswith('.pdf'):
+        page_title = page_url.split('/')[-1].split('.')[0]
+    else:
+        try:
+            with requests.get(page_url) as page_response:
+                parsed_page = BeautifulSoup(page_response.text, 'html.parser')
+            page_title = parsed_page.title.text
+        except:
+            page_title = page_url
 
-    # TODO save item to Trello
+    card = trello_list.add_card(name=page_title,
+                                desc=pocket_item_data['excerpt'])
+    card.attach(url=f'https://getpocket.com/a/read/{pocket_item_id}')
+    card.attach(url=pocket_item_data['given_url'])
 
 
 conf_data['pocket_last_checked'] = now_timestamp
