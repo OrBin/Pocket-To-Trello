1478a8b98fc4cca22040c10f993d6297e34f4fd7
Or Bin <orbin50@gmail.com>
2018-08-12 19:07:11 +0300

    Added logging

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import sys
 import json
 import requests
 import os
+import logging
+from logging.handlers import RotatingFileHandler
 from pocket import Pocket
 from datetime import datetime
 from bs4 import BeautifulSoup
@@ -9,7 +11,20 @@ from trello import TrelloClient
 
 
 CONFIG_FILE_NAME = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'config.json')
+LOG_FILE_NAME = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'log.txt')
 AUTH_DATA_KEY = 'authentication'
+
+
+# Enable logging
+file_handler = RotatingFileHandler(
+    LOG_FILE_NAME, maxBytes=(1048576 * 5), backupCount=7
+)
+console_handler = logging.StreamHandler()
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO,
+                    handlers=[file_handler, console_handler])
+logger = logging.getLogger(__name__)
 
 # Reading configuration and authentication data
 with open(CONFIG_FILE_NAME, 'r') as conf_file:
@@ -20,6 +35,7 @@ auth_data = conf_data[AUTH_DATA_KEY]
 pocket_consumer_key = conf_data[AUTH_DATA_KEY]['pocket_consumer_key']
 pocket_client = Pocket(conf_data[AUTH_DATA_KEY]['pocket_consumer_key'],
                        conf_data[AUTH_DATA_KEY]['pocket_user_credentials']['access_token'])
+logger.info('Logged in to Pocket')
 
 # Logging in to Trello
 trello_client = TrelloClient(
@@ -27,20 +43,23 @@ trello_client = TrelloClient(
     token='8a3e786371ceeba2d9f650a9a587772fce41a6fc7dccc7ad1747b6c402982494'
 )
 trello_list = trello_client.get_list(conf_data['trello_list_id'])
+logger.info('Logged in to Trello')
 
 now_timestamp = int(datetime.now().timestamp())
 since_timestamp = conf_data['pocket_last_checked'] if 'pocket_last_checked' in conf_data else now_timestamp
 
 new_pocket_items, _ = pocket_client.get(since=since_timestamp)
+logger.info('Fetched new Pocket items')
 
 if len(new_pocket_items['list']) == 0:
-    print('No new items.')
+    logger.info('No new items.')
     sys.exit(0)
 
 for pocket_item_id, pocket_item_data in new_pocket_items['list'].items():
+    page_url = pocket_item_data['given_url']
+    logger.info(f'Found item {page_url}')
 
     # Getting page title
-    page_url = pocket_item_data['given_url']
     if page_url.endswith('.pdf'):
         page_title = page_url.split('/')[-1].split('.')[0]
     else:
@@ -53,8 +72,12 @@ for pocket_item_id, pocket_item_data in new_pocket_items['list'].items():
 
     card = trello_list.add_card(name=page_title,
                                 desc=pocket_item_data['excerpt'])
-    card.attach(url=f'https://getpocket.com/a/read/{pocket_item_id}')
-    card.attach(url=pocket_item_data['given_url'])
+    logger.info(f'Created card \'{page_title}\')')
+    pocket_item_url = f'https://getpocket.com/a/read/{pocket_item_id}'
+    card.attach(url=pocket_item_url)
+    logger.info(f'Attached link {pocket_item_url} to item')
+    card.attach(url=page_url)
+    logger.info(f'Attached link {page_url} to item')
 
 
 conf_data['pocket_last_checked'] = now_timestamp
