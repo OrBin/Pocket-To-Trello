566fd7d63a4c2cb3a75b3ac9167a9202f5fb8429
Or Bin <orbin50@gmail.com>
2022-12-22 02:13:32 +0200

    Refactored script: extracted most of the code to functions

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
-import sys
 import json
+from typing import Dict, Any
 import requests
 import logging
-import os
-from os.path import join, dirname, abspath
-from logging.handlers import RotatingFileHandler
+from os.path import dirname, abspath
+from pathlib import Path
 from pocket import Pocket
 from datetime import datetime
 from bs4 import BeautifulSoup
 from trello import TrelloClient
 
 
-CONFIG_FILE_NAME = join(dirname(abspath(__file__)), 'config.json')
+CONFIG_FILE_NAME = Path(dirname(abspath(__file__))) / 'config.json'
 AUTH_DATA_KEY = 'authentication'
 
 
@@ -20,24 +19,69 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     handlers=[logging.StreamHandler()])
 logger = logging.getLogger(__name__)
 
+def load_config() -> Dict[str, Any]:
+    with open(CONFIG_FILE_NAME, 'r') as conf_file:
+        conf_data = json.load(conf_file)
+    return conf_data
+
+
+def load_pocket_client(conf_data: Dict[str, Any]) -> Pocket:
+    pocket_client = Pocket(
+        conf_data[AUTH_DATA_KEY]['pocket_consumer_key'],
+        conf_data[AUTH_DATA_KEY]['pocket_user_credentials']['access_token'],
+    )
+    logger.info('Logged in to Pocket')
+    return pocket_client
+
+
+def load_trello_client(conf_data: Dict[str, Any]) -> TrelloClient:
+    trello_client = TrelloClient(
+        api_key=conf_data[AUTH_DATA_KEY]['trello_api_key'],
+        token=conf_data[AUTH_DATA_KEY]['trello_token']
+    )
+    logger.info('Logged in to Trello')
+    return trello_client
+
+
+def convert_single_item(pocket_item_id, pocket_item_data):
+    # status - 0, 1, 2 - 1 if the item is archived - 2 if the item should be deleted
+    if not pocket_item_data['status'] == '0':
+        return
+
+    # The 'since' parameter on Pocket.get relates to update time only, but we want to filter by add time
+    item_add_timestamp = int(pocket_item_data['time_added'])
+    if item_add_timestamp < since_timestamp:
+        return
+
+    page_url = pocket_item_data['given_url']
+    logger.info(f'Found item {page_url}')
+
+    # Getting page title
+    if page_url.endswith('.pdf'):
+        page_title = page_url.split('/')[-1].split('.')[0]
+    else:
+        try:
+            with requests.get(page_url) as page_response:
+                parsed_page = BeautifulSoup(page_response.text, 'html.parser')
+            page_title = parsed_page.title.text
+        except:
+            page_title = page_url
+
+    card = trello_list.add_card(name=page_title,
+                                desc=pocket_item_data['excerpt'])
+    logger.info(f'Created card \'{page_title}\')')
+    card.attach(name='Original article', url=page_url)
+    card.attach(name='Pocket (web)', url=f'https://app.getpocket.com/read/{pocket_item_id}')
+    card.attach(name='Pocket (mobile)', url=f'https://getpocket.com/a/read/{pocket_item_id}')
+    logger.info(f'Attached links to card')
+
+
 # Reading configuration and authentication data
-with open(CONFIG_FILE_NAME, 'r') as conf_file:
-    conf_data = json.load(conf_file)
-auth_data = conf_data[AUTH_DATA_KEY]
+conf_data = load_config()
+pocket_client = load_pocket_client(conf_data)
+trello_client = load_trello_client(conf_data)
 
-# Logging in to Pocket
-pocket_consumer_key = conf_data[AUTH_DATA_KEY]['pocket_consumer_key']
-pocket_client = Pocket(conf_data[AUTH_DATA_KEY]['pocket_consumer_key'],
-                       conf_data[AUTH_DATA_KEY]['pocket_user_credentials']['access_token'])
-logger.info('Logged in to Pocket')
-
-# Logging in to Trello
-trello_client = TrelloClient(
-    api_key=conf_data[AUTH_DATA_KEY]['trello_api_key'],
-    token=conf_data[AUTH_DATA_KEY]['trello_token']
-)
 trello_list = trello_client.get_list(conf_data['trello_list_id'])
-logger.info('Logged in to Trello')
 
 now_timestamp = int(datetime.now().timestamp())
 since_timestamp = conf_data['pocket_last_checked'] if 'pocket_last_checked' in conf_data else now_timestamp
@@ -49,37 +93,7 @@ if len(new_pocket_items['list']) == 0:
     logger.info('No new items.')
 else:
     for pocket_item_id, pocket_item_data in new_pocket_items['list'].items():
-
-        # status - 0, 1, 2 - 1 if the item is archived - 2 if the item should be deleted
-        if not pocket_item_data['status'] == '0':
-            continue
-
-        # The 'since' parameter on Pocket.get relates to update time only, but we want to filter by add time
-        item_add_timestamp = int(pocket_item_data['time_added'])
-        if item_add_timestamp < since_timestamp:
-            continue
-
-        page_url = pocket_item_data['given_url']
-        logger.info(f'Found item {page_url}')
-
-        # Getting page title
-        if page_url.endswith('.pdf'):
-            page_title = page_url.split('/')[-1].split('.')[0]
-        else:
-            try:
-                with requests.get(page_url) as page_response:
-                    parsed_page = BeautifulSoup(page_response.text, 'html.parser')
-                page_title = parsed_page.title.text
-            except:
-                page_title = page_url
-
-        card = trello_list.add_card(name=page_title,
-                                    desc=pocket_item_data['excerpt'])
-        logger.info(f'Created card \'{page_title}\')')
-        card.attach(name='Original article', url=page_url)
-        card.attach(name='Pocket (web)', url=f'https://app.getpocket.com/read/{pocket_item_id}')
-        card.attach(name='Pocket (mobile)', url=f'https://getpocket.com/a/read/{pocket_item_id}')
-        logger.info(f'Attached links to card')
+        convert_single_item(pocket_item_id, pocket_item_data)
 
 conf_data['pocket_last_checked'] = now_timestamp
 with open(CONFIG_FILE_NAME, 'w') as conf_file:
